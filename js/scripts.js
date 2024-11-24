9fb8849ebe69ebde3fb2c40fe197ccfcd37762a5
vimalchacko <vimalvchacko@gmail.com>
2024-11-24 00:31:23 -0500

    logo and calendar

--- js/scripts.js
+++ js/scripts.js
@@ -189,20 +189,20 @@ $(document).ready(function () {
             title: "Nandhitha and Vimal's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jan 4, 2025 11:30'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jan 4, 2025 14:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'St. Thomas Syro-Malabar Catholic Forane Church, 115 Ionview Road, Scarborough, ON M1K 3A1, Canada',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Roy David at +1 226-899-0083."
         }
     });
 
