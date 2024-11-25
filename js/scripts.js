4b6e1e2e91f4b58b473ff4d25d783788f3672653
vimalchacko <vimalvchacko@gmail.com>
2024-11-25 15:00:36 -0500

    fixes

--- js/scripts.js
+++ js/scripts.js
@@ -93,7 +93,7 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "35px 0"
+                    "padding": "25px 0"
                 });
                 $('header .member-actions').css({
                     "top": "25px",
@@ -105,7 +105,7 @@ $(document).ready(function () {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "35px 0"
+                    "padding": "25px 0"
                 });
                 $('header .member-actions').css({
                     "top": "25px",
