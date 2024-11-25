1608a0e3d475c712a8a04aee84261b683a24ed1b
vimalchacko <vimalvchacko@gmail.com>
2024-11-25 14:34:05 -0500

    fixes

--- js/scripts.js
+++ js/scripts.js
@@ -96,22 +96,22 @@ $(document).ready(function () {
                     "padding": "35px 0"
                 });
                 $('header .member-actions').css({
-                    "top": "26px",
+                    "top": "25px",
                 });
                 $('header .navicon').css({
-                    "top": "34px",
+                    "top": "35px",
                 });
             } else {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
+                    "padding": "35px 0"
                 });
                 $('header .member-actions').css({
-                    "top": "41px",
+                    "top": "25px",
                 });
                 $('header .navicon').css({
-                    "top": "48px",
+                    "top": "35px",
                 });
             }
         });
